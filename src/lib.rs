26426043ada9c42a06ad19199db9309e1e271bea
Cyril Plisko <cyril.plisko@mountall.com>
2018-04-09 12:13:31 +0300

    First drop

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,127 @@
+#![cfg_attr(all(feature = "cargo-clippy", feature = "pedantic"), warn(clippy_pedantic))]
+#![cfg_attr(feature = "cargo-clippy", warn(use_self))]
+#![deny(warnings, missing_debug_implementations)]
+
+extern crate libc;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+use std::slice;
+use std::vec;
+
+use libc::{c_int, iovec};
+use serde::de::{self, Error};
+use serde::ser::{SerializeSeq, SerializeTupleVariant};
+use serde::{Serialize, Serializer};
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub enum SgData {
+    SgList(SgList),
+    SgVec(Vec<Vec<u8>>),
+    Direct(Vec<u8>),
+}
+
+impl From<SgList> for SgData {
+    fn from(sglist: SgList) -> Self {
+        SgData::SgList(sglist)
+    }
+}
+
+impl From<Vec<u8>> for SgData {
+    fn from(vec: Vec<u8>) -> Self {
+        SgData::Direct(vec)
+    }
+}
+
+impl From<Vec<Vec<u8>>> for SgData {
+    fn from(sgvec: Vec<Vec<u8>>) -> Self {
+        SgData::SgVec(sgvec)
+    }
+}
+
+impl Serialize for SgData {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match *self {
+            SgData::SgList(ref sglist) => {
+                // While serializing `SgList` variant mimics `SgVec`.
+                let mut data = serializer.serialize_tuple_variant("SgData", 1, "SgVec", 1)?;
+                data.serialize_field(sglist)?;
+                data.end()
+            }
+            SgData::SgVec(ref sgvec) => {
+                let mut data = serializer.serialize_tuple_variant("SgData", 1, "SgVec", 1)?;
+                data.serialize_field(sgvec)?;
+                data.end()
+            }
+            SgData::Direct(ref buf) => {
+                let mut data = serializer.serialize_tuple_variant("SgData", 2, "Direct", 1)?;
+                data.serialize_field(buf)?;
+                data.end()
+            }
+        }
+    }
+}
+
+impl IntoIterator for SgData {
+    type Item = Vec<u8>;
+    type IntoIter = vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let vec = match self {
+            SgData::SgList(_) => unimplemented!(),
+            SgData::SgVec(sgvec) => sgvec,
+            SgData::Direct(buf) => vec![buf],
+        };
+
+        vec.into_iter()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct SgList {
+    iovec: *const iovec,
+    count: c_int,
+}
+
+impl SgList {
+    pub fn new(iovec: *const iovec, count: c_int) -> Self {
+        Self { iovec, count }
+    }
+}
+
+impl Serialize for SgList {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let count = self.count as usize;
+        let mut seq = serializer.serialize_seq(Some(count))?;
+        for idx in 0..self.count as isize {
+            let buf = unsafe {
+                let iov = self.iovec.offset(idx);
+                let base = (*iov).iov_base as *const u8;
+                let len = (*iov).iov_len as usize;
+                slice::from_raw_parts(base, len)
+            };
+            seq.serialize_element(buf)?;
+        }
+        seq.end()
+    }
+}
+
+impl<'de> de::Deserialize<'de> for SgList {
+    fn deserialize<D>(_deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        Err(D::Error::custom("Cannot deserialize SgList"))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ tests/data.rs
@@ -0,0 +1,120 @@
+extern crate bincode;
+extern crate libc;
+extern crate serde;
+extern crate serde_test;
+extern crate sgdata;
+
+use std::mem;
+
+use bincode::{deserialize, serialize};
+use libc::{c_int, c_void, iovec};
+use serde_test::{assert_ser_tokens, Token};
+use sgdata::{SgData, SgList};
+
+fn vec_into_iovec(mut vec: Vec<u8>) -> iovec {
+    let len = vec.len();
+    let base = vec.as_mut_ptr();
+    mem::forget(vec);
+    iovec {
+        iov_base: base as *mut c_void,
+        iov_len: len,
+    }
+}
+
+fn create_sglist(sgvec: Vec<Vec<u8>>) -> SgList {
+    let vec = sgvec.into_iter().map(vec_into_iovec).collect::<Vec<_>>();
+    let len = vec.len();
+    let iov = vec.as_ptr();
+    mem::forget(vec);
+    SgList::new(iov, len as c_int)
+}
+
+#[test]
+fn sglist_serialize_deserialize() {
+    let sgvec = vec![vec![0x45_u8; 4096]; 5];
+    let data = SgData::from(create_sglist(sgvec));
+    let buf = serialize(&data).unwrap();
+    let data: SgData = deserialize(&buf).unwrap();
+
+    assert_eq!(data, SgData::from(vec![vec![0x45_u8; 4096]; 5]));
+}
+
+#[test]
+fn direct() {
+    let data: SgData = vec![12, 56, 34, 255, 0].into();
+
+    assert_ser_tokens(
+        &data,
+        &[
+            Token::TupleVariant {
+                name: "SgData",
+                variant: "Direct",
+                len: 1,
+            },
+            Token::Seq { len: Some(5) },
+            Token::U8(12),
+            Token::U8(56),
+            Token::U8(34),
+            Token::U8(255),
+            Token::U8(0),
+            Token::SeqEnd,
+            Token::TupleVariantEnd,
+        ],
+    );
+}
+
+#[test]
+fn sgvec() {
+    let data: SgData = vec![vec![12, 56, 76], vec![128, 255]].into();
+
+    assert_ser_tokens(
+        &data,
+        &[
+            Token::TupleVariant {
+                name: "SgData",
+                variant: "SgVec",
+                len: 1,
+            },
+            Token::Seq { len: Some(2) },
+            Token::Seq { len: Some(3) },
+            Token::U8(12),
+            Token::U8(56),
+            Token::U8(76),
+            Token::SeqEnd,
+            Token::Seq { len: Some(2) },
+            Token::U8(128),
+            Token::U8(255),
+            Token::SeqEnd,
+            Token::SeqEnd,
+            Token::TupleVariantEnd,
+        ],
+    );
+}
+
+#[test]
+fn sglist() {
+    let data: SgData = create_sglist(vec![vec![12, 56, 76], vec![128, 255]]).into();
+
+    assert_ser_tokens(
+        &data,
+        &[
+            Token::TupleVariant {
+                name: "SgData",
+                variant: "SgVec",
+                len: 1,
+            },
+            Token::Seq { len: Some(2) },
+            Token::Seq { len: Some(3) },
+            Token::U8(12),
+            Token::U8(56),
+            Token::U8(76),
+            Token::SeqEnd,
+            Token::Seq { len: Some(2) },
+            Token::U8(128),
+            Token::U8(255),
+            Token::SeqEnd,
+            Token::SeqEnd,
+            Token::TupleVariantEnd,
+        ],
+    );
+}
